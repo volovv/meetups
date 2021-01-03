675b980fa3bb9e01a0dc1fce5e9a0b4577123d01
Viktoria Vologzhanina <vologzhaninavv@gmail.com>
2021-01-03 23:17:04 +0300

    Changed rules for quotes from 2 to 1

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     parser: "babel-eslint"
   },
   rules: {
+    "quotes": 1,
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
   }
