26b09d85000a47c0ed51103df76404a7717c1451
Viktoria Vologzhanina <vologzhaninavv@gmail.com>
2021-01-31 01:26:15 +0300

    Аdded methods for working with images, meetup data and changed name of service API

---
+++ src/imageService.js
@@ -0,0 +1,27 @@
+/** URL адрес API */
+export const API_URL = "https://course-vue.javascript.ru/api";
+
+export const ImageService = {
+  /**
+   * Загружает файл на сервер и получает
+   * https://course-vue.javascript.ru/api/#/Images/ImagesController_uploadImage
+   * @param {File} file - файл, который требуется загрузить
+   * @return {Promise<Object>} - объект с ID изображения
+   */
+  // eslint-disable-next-line no-unused-vars
+  uploadImage(file) {
+    return fetch(`${API_URL}/images/upload`, {
+      method: "POST",
+      body: file
+    }).then(res => res.json());
+  },
+
+  /**
+   * Возвращает ссылку на изображение по ID
+   * @param id - ID изображения
+   * @return {string} - ссылка на изображение
+   */
+  getImageURL(id) {
+    return id !== null ? `${API_URL}/images/${id}` : null;
+  }
+};
